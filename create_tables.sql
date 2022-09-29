c83f74823e1511c4a8288b3f780d94a8a7b504f8
gramdel <legovec96@gmail.com>
2022-09-29 19:16:58 +0300

    Добавил ON DELETE там, где надо

--- create_tables.sql
+++ create_tables.sql
@@ -13,7 +13,7 @@ CREATE TABLE city (
     name TEXT NOT NULL UNIQUE,
     population BIGINT NOT NULL,
     city_type BIGINT REFERENCES city_type(id) NOT NULL,
-    location BIGINT REFERENCES location(id) NOT NULL UNIQUE
+    location BIGINT REFERENCES location(id) ON DELETE CASCADE NOT NULL UNIQUE
 );
 
 CREATE TABLE job (
@@ -36,20 +36,20 @@ CREATE TABLE citizen (
     name TEXT NOT NULL UNIQUE,
     age INT NOT NULL,
     reincarnation_count int,
-    city BIGINT REFERENCES city(id) NOT NULL,
-    job BIGINT REFERENCES job(id),
-    doubt BIGINT REFERENCES doubt(id),
-    expectation BIGINT REFERENCES expectation(id)
+    city BIGINT REFERENCES city(id) ON DELETE CASCADE NOT NULL,
+    job BIGINT REFERENCES job(id) ON DELETE SET NULL,
+    doubt BIGINT REFERENCES doubt(id) ON DELETE SET NULL,
+    expectation BIGINT REFERENCES expectation(id) ON DELETE SET NULL
 );
 
 CREATE TABLE technology (
     id BIGSERIAL PRIMARY KEY,
     name TEXT NOT NULL UNIQUE,
-    expectation BIGINT REFERENCES expectation(id)
+    expectation BIGINT REFERENCES expectation(id) ON DELETE SET NULL
 );
 
 CREATE TABLE citizen_technology (
-    citizen BIGINT REFERENCES citizen(id) NOT NULL,
-    technology BIGINT REFERENCES technology(id) NOT NULL,
+    citizen BIGINT REFERENCES citizen(id) ON DELETE CASCADE NOT NULL,
+    technology BIGINT REFERENCES technology(id) ON DELETE CASCADE NOT NULL,
     PRIMARY KEY (citizen, technology)
 );
